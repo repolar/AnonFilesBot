a90a6403e6e4b5cf07896e4c202b9da5d9b33840
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-06-05 18:24:41 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+# Infinity Bots <https://t.me/Infinity_Bots)
+# ImJanindu
+
+import os
+import sys
+import logging
+import pyrogram
+import asyncio
+from config import Config
+from pyrogram import Client, idle, filters
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# vars
+APP_ID = Config.APP_ID
+API_HASH = Config.API_HASH
+BOT_TOKEN = Config.BOT_TOKEN
+
+    
+bot = Client(
+    "AnonFilesBot",
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="AnonFilesBot/plugins")
+)
+
+
+
+bot.start()
+print("Anon Files Bot Started!/n/nVisit @JEBotZ.")
+
+
+idle()
+bot.stop()
