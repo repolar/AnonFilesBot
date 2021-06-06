49e4c3626d924d7a23de006dcaa93f74d82aeb35
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-06-06 10:51:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ import pyrogram
 import aiohttp
 import asyncio
 import requests
-from random import randit
+import randit
 from progress import progress
 from config import Config
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel
